48ac4c64c2cfb581e01d3e31fcc2daaf276eef3f
Jawad Dhew <jawaddhew98@gmail.com>
2022-11-09 18:31:07 +0600

    review dne

--- index.js
+++ index.js
@@ -77,6 +77,13 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
         // app.post('/orders', async (req, res) => {
         //     const order = req.body;
         //     const result = await orderCollection.insertOne(order);
