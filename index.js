67059e908dfd82e1f7847d29f574b126c1185c1b
Jawad Dhew <jawaddhew98@gmail.com>
2022-11-09 21:24:00 +0600

    service added

--- index.js
+++ index.js
@@ -77,6 +77,12 @@ async function run() {
             res.send(result);
         });
 
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        });
+
         app.delete('/reviews/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
